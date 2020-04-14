19d5d12c6241ce352c2fd9112a3945d22281c98d
pingzilao <pingzilao@qq.com>
2020-04-14 22:19:01 +0800

    helpuser adjust time

--- tasker/helpuser_tasker.go
+++ tasker/helpuser_tasker.go
@@ -58,7 +58,7 @@ func (this *Tasker) jtydhkHelpUserWork() {
 	for ;true; {
 		conds := []*models.SqlPairCondition{
 			&models.SqlPairCondition{"id > ?", startId},
-			&models.SqlPairCondition{"created_at >= ?", time.Now().Unix() - 130*3600},
+			&models.SqlPairCondition{"created_at >= ?", time.Now().Unix() - 30*24*3600},
 			&models.SqlPairCondition{"status = ?", models.CONST_OrderStatus_HelpUser_Apply_Doing},
 			&models.SqlPairCondition{"third_order_at <= ?", time.Now().Unix() - 4*3600},
 		}
